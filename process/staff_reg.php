006e325971638f7250709957e8c12af50effc06f
Idepefo Olakunle Matthew <dokiiworld.ik@gmail.com>
2021-10-15 13:06:07 +0100

    Email notification added to staff registration

--- process/staff_reg.php
+++ process/staff_reg.php
@@ -19,13 +19,13 @@ $fise = $_FILES['image']['name'];
 $fiso = $_FILES['image']['type'];
 $fisa = $_FILES['image']['size'];
 $fisu = $_FILES['image']['tmp_name'];
-$vmag= $fn.$em.$fise;
+$vmag= $fn.$pn.$fise;
 $sof=move_uploaded_file($_FILES['image']['tmp_name'],'../img/staff_passport/'.$vmag);
 $status="active";
   // echo json_encode($fn.''.$em.''.$vmag);
 function staffreg($conn,$fn,$ln,$em,$pn,$ph,$vmag,$pass,$status)
 		{
-			$sa= $conn->prepare("SELECT * from staff where email='$em'");
+			$sa= $conn->prepare("SELECT * from staff where email='$em' or phone='$pn'");
 			if($sa->execute())
 			{				
 				$gi=$sa->get_result();
@@ -41,32 +41,41 @@ function staffreg($conn,$fn,$ln,$em,$pn,$ph,$vmag,$pass,$status)
 					$cm->bind_param("ssssssss",$fn,$ln,$em,$pn,$ph,$vmag,$pass,$status);
 					if($cm->execute())			
 					{
-											// $to ="$em";
-											// $subject="Registration successful | vportal";
-											// $message="
-											// <html>
-											// 	<body>														
-											// 		<h2>Staff Registration | vportal</h2>					
-											// 		<p>Hello $fn $ln, <br> Your account has been successfully created. Below are your login details</p>
-											// 		<table>
-											// 			<tr>
-											// 			<th>Email:</th><td><b>$em</b></td>
-											// 			</tr>	
-											// 			<tr>
-											// 			<th>Password:</th><td><b>$pass</b></td>
-											// 			</tr>										
-											// 		</table>
-											// 		<p>Proceed to <a href='https://nutportal.webmatt.com.ng'><button class='btn btn-info'>Login</button></a> </p>											
-											// 		<p>Thank You!</p>
-											// 		<p>Cheers,<br><a href='https://vportal.ng'>V-Portal Team.</a></p>
-											// 	</body>
-											// </html>
-											// ";
-											// $headers= "MIME-Version: 1.0" . "\r\n";
-											// $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
-											// $headers .= 'From: <info@vportal.ng>' . "\r\n";
+											$to ="$em";
+											$subject="Admin Registration Successful!";
+											$message="
+											<html>
+											
+										
+										<div style='padding:5px'>
+										<p>Hello $fn $ln,</p>
+																
+										<p>Your Admin portal at SUG-V-PORTAL has been successfully created.
+										</p>
+										<p>Below are your login details<br>
+											Email:<b>$em</b><br>Password:<b>$pass</b><br>
+											<em>Make sure you keep your details safe to yourself</em>
+										</p>
+										<p>
+										Click <a href='https://sugvportal.ezamatt.com'><button>LOGIN</button></a> to login and manage your page.
+										</p>
+																			   
+										<p>Thank you.</p>
+										
+										<p>Cheers!<br>
+										<b><a href='https://sugvportal.ezamatt.com'>SUG-V-PORTAL Team.</a></b>
+										</p>
+										</div>			    
+									   
+										
+											</body>
+											</html>
+											";
+											$headers= "MIME-Version: 1.0" . "\r\n";
+											$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+											$headers .= 'From: SUG-V-PORTAL <info@sugvportal.ezamatt.com>' . "\r\n";
 
-											// mail($to,$subject,$message,$headers);
+											mail($to,$subject,$message,$headers);
 							echo json_encode('saved');
 					}
 					else
